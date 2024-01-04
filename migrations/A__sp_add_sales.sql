c4f51516926924345f8c6fe24a7a67f7284d6ec2
DTAP598 <ashish.learning598@gmail.com>
2024-01-04 15:27:42 +0530

    Update A__sp_add_sales.sql

--- migrations/A__sp_add_sales.sql
+++ migrations/A__sp_add_sales.sql
@@ -4,3 +4,6 @@ CREATE TABLE sale_table
    order_no VARCHAR
   ,price VARCHAR
 );
+ALTER TABLE sale_table ADD COLUMN date datetime;
+INSERT INTO demo.sale_table (order_no, price,date)
+    VALUES ('a', 'b','2024-01-01');
