57d5d87a8e67b0e78b4cb16256d2d301b58c3205
DTAP598 <ashish.learning598@gmail.com>
2024-01-05 15:42:36 +0530

    deletes

--- atnt/V4.1.4__sample_view.sql
+++
@@ -1,4 +0,0 @@
-use schema DEMO_DB;
-use schema DEMO;
-create or replace view sample_view as 
-select * from HELLO_WORLD;
--- atnt/V5.1.1__sample_data.sql
+++
@@ -1,6 +0,0 @@
-use schema ATNT_DEMO_SCHEMA;
-CREATE TABLE sample_data
-(
-   a VARCHAR
-  ,b VARCHAR
-);
