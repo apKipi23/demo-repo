09b576623f77383efa03db3945b2e73f90889a52
DTAP598 <ashish.learning598@gmail.com>
2024-01-05 14:53:53 +0530

    Create V4.1.1__salesdata.sql

---
+++ atnt/V4.1.1__salesdata.sql
@@ -0,0 +1,9 @@
+CREATE or replace TABLE sales_table
+(
+   order_no VARCHAR
+  ,price VARCHAR
+  , order_date datetime
+);
+
+INSERT INTO sales_table (order_no, price,order_date)
+    VALUES ('a', 'b','2024-01-01');
